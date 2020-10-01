20f34ca50f4928c128e80c8ab9999b1e4ef8ea20
Mike Wittie <mwittie@cs.montana.edu>
2020-10-01 14:21:09 +0200

    add a UDP example

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
-import requests
+import socket
 
-r = requests.get('http://localhost:8000/file.html')
-print(r.text)
+# open an IP (AF_INET) USP (SOCK_DGRAM) socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send bytes to localhost address and port 5000 from a utf-g encoded string
+s.sendto('hello'.encode('utf-8'), ('127.0.0.1', 5000))
--- server.py
+++ server.py
@@ -1,56 +1,13 @@
-import email
-import io
-import mimetypes
-import os
-import posixpath
-import shutil
-import urllib
-import datetime
-import html
-import sys
-from http import HTTPStatus
-from http.server import SimpleHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler
+import socket
 
+# open an IP (AF_INET) UDP (SOCK_DGRAM) socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
-	"""A custom HTTP Request Handler based on SimpleHTTPRequestHandler"""
-	
-	server_version = "My_HTTP_Server/"
-	
-	def __init__(self, *args, directory=None, **kwargs):
-		if directory is None:
-			directory = os.getcwd()    # start file path with the current directory
-		self.directory = directory    # or with the directory passed in the argument
-		super().__init__(*args, **kwargs)   # initialize the base handler
-	
-	def do_GET(self):
-		"""Serve a GET request."""
-		
-		# get info from the HTTP request
-		# look at https://docs.python.org/3/library/http.server.html for other BaseHTTPRequestHandler instance variables
-		print(self.client_address)
-		print(self.path)
+# bind the socket to localhost address and port 5000
+s.bind(('127.0.0.1', 5000))
 
-		# create an HTTP response
-		# look for different responses by trying different completions of HTTPStatus.
-		self.send_response(HTTPStatus.OK)
-		self.end_headers()
-		
-		# send the requested file
-		f = open(self.path.strip('/'), 'rb')   # open the requested file
-		try:
-			shutil.copyfileobj(f, self.wfile)   # copy the file for inclusion in the response
-		finally:
-			f.close()
-	
+# receive up to 64 bytes of data from the socket buffer
+data, addr = s.recvfrom(64)
 
-
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-	server_address = ('', 8000)
-	httpd = server_class(server_address, handler_class)
-	httpd.serve_forever()
-
-
-
-if __name__ == "__main__":
-	run(HTTPServer, MyHTTPRequestHandler)
+# print out the received message after decoring the string from utf-8 encoded bytes
+print("Received message: '" + data.decode('utf-8') + "'")
