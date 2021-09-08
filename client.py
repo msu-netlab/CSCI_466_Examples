07ea860f1db3b139e59b799fbd74560e90b8367f
Mike Wittie <mwittie@blocky.rocks>
2021-09-08 11:53:04 -0600

    Update example to return different codes

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import requests
 
 r = requests.get('http://localhost:8000/file.html')
-print(r.text)
+print('Reply status code %s\nContent:\n%s\n\n' % (r.status_code, r.text))
+
+r = requests.get('http://localhost:8000/non_existent_file.html')
+print('Reply status code %s\nContent:\n%s\n\n' % (r.status_code, r.text))
--- server.py
+++ server.py
@@ -1,56 +1,49 @@
-import email
-import io
-import mimetypes
 import os
-import posixpath
 import shutil
-import urllib
-import datetime
-import html
-import sys
 from http import HTTPStatus
 from http.server import SimpleHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler
 
 
 class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
-	"""A custom HTTP Request Handler based on SimpleHTTPRequestHandler"""
-	
-	server_version = "My_HTTP_Server/"
-	
-	def __init__(self, *args, directory=None, **kwargs):
-		if directory is None:
-			directory = os.getcwd()    # start file path with the current directory
-		self.directory = directory    # or with the directory passed in the argument
-		super().__init__(*args, **kwargs)   # initialize the base handler
-	
-	def do_GET(self):
-		"""Serve a GET request."""
-		
-		# get info from the HTTP request
-		# look at https://docs.python.org/3/library/http.server.html for other BaseHTTPRequestHandler instance variables
-		print(self.client_address)
-		print(self.path)
-
-		# create an HTTP response
-		# look for different responses by trying different completions of HTTPStatus.
-		self.send_response(HTTPStatus.OK)
-		self.end_headers()
-		
-		# send the requested file
-		f = open(self.path.strip('/'), 'rb')   # open the requested file
-		try:
-			shutil.copyfileobj(f, self.wfile)   # copy the file for inclusion in the response
-		finally:
-			f.close()
-	
+    """A custom HTTP Request Handler based on SimpleHTTPRequestHandler"""
+    
+    server_version = "My_HTTP_Server/"
+    path_prefix = "www"
+    
+    def __init__(self, *args, directory=None, **kwargs):
+        if directory is None:
+            directory = os.getcwd()  # start file path with the current directory
+        self.directory = directory  # or with the directory passed in the argument
+        super().__init__(*args, **kwargs)  # initialize the base handler
+    
+    def do_GET(self):
+        """Serve a GET request."""
+        
+        # get info from the HTTP request
+        # look at https://docs.python.org/3/library/http.server.html for other BaseHTTPRequestHandler instance variables
+        print(self.client_address)
+        print(self.path)
+        
+        # update the path with the prefix of server files
+        self.path = self.path_prefix + self.path
+        
+        # reply to client
+        try:
+            f = open(self.path, 'rb')
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            shutil.copyfileobj(f, self.wfile)
+            f.close()
+        except OSError:
+            self.send_response(HTTPStatus.NOT_FOUND)
+            self.end_headers()
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-	server_address = ('', 8000)
-	httpd = server_class(server_address, handler_class)
-	httpd.serve_forever()
-
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
-	run(HTTPServer, MyHTTPRequestHandler)
+    run(HTTPServer, MyHTTPRequestHandler)
